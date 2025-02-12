0befe662f73e228b8bb9f705f644327b389920e4
imilwkk <milena.subanova@alatoo.edu.kg>
2025-02-12 12:39:30 +0600

    first commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main  {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+       //ask about minimal and maximal length
+        System.out.print("Minimal password length: ");
+        int minLength = scanner.nextInt();
+
+        System.out.print("Maximal length: ");
+        int maxLength = scanner.nextInt();
+        //ask about lowercase letters and uppercase letters
+        System.out.print("use lowercase letters: yes or not? ");
+
+
+
+        }
+    }
+}
